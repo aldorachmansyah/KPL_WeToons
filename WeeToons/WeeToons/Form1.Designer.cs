8f26b5452f4d0d32e522528a6725574f02a6c227
windadwiastini <winda1996@hotmail.com>
2017-11-29 10:37:36 +0700

    testing

--- WeeToons/WeeToons/Form1.Designer.cs
+++ WeeToons/WeeToons/Form1.Designer.cs
@@ -3,7 +3,7 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// Required designer variable. tes
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
