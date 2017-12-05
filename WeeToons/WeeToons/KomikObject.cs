ce504ffb443be5050ffa4292622950ea1afef2ce
deniismail <deniismail321@gmail.com>
2017-12-05 22:01:49 +0700

    initiate class KomikObject

--- WeeToons/WeeToons/KomikObject.cs
+++ WeeToons/WeeToons/KomikObject.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace WeeToons
 {
-    class KomikObject
+    abstract class KomikObject
     {
+        public abstract bool Add(KomikObject obj);
+        public abstract bool Remove(KomikObject obj);
+
+        public abstract bool Intersect(int xTest, int yTest);
+        public abstract void Translate(int x, int y, int xAmount, int yAmount);
+
+            
     }
 }
