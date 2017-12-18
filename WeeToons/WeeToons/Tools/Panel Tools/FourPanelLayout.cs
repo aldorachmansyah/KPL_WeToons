325061790cec6066ff34afbb0838897223cf8573
Wira Mahardika <putudikka@gmail.com>
2017-12-19 01:58:59 +0700

    Add 4 panel layout tool

---
+++ WeeToons/WeeToons/Tools/Panel Tools/FourPanelLayout.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeeToons.Interfaces;
+using WeeToons.Tools.Selection_Tools;
+
+namespace WeeToons.Tools.Panel_Tools
+{
+    class FourPanelLayout : ToolStripMenuItem, ITool
+    {
+        private IPanelContainer panelContainer;
+
+        public IPanelContainer PanelContainer
+        {
+            get
+            {
+                return this.panelContainer;
+            }
+
+            set
+            {
+                this.panelContainer = value;
+            }
+        }
+
+        public FourPanelLayout()
+        {
+            this.Text = "4 Panel";
+            this.Name = "fourPanelToolStrip";
+            this.Click += new EventHandler(this.tool_Click);
+        }
+
+        public void tool_Click(object sender, EventArgs e)
+        {
+            this.panelContainer.RemoveAllPanel();
+            this.AddPanel(10, 20, 310, 310, "Top Left Quartet Panel");
+            this.AddPanel(325, 20, 310, 310, "Top Right Quartet Panel");
+            this.AddPanel(10, 335, 310, 310, "Bottom Left Quartet Panel");
+            this.AddPanel(325, 335, 310, 310, "Bottom Right Quartet Panel");
+            this.panelContainer.Text = "4 Panel";
+        }
+
+        private void AddPanel(int xPosition, int yPosition, int width, int height, string panelName = "Unknown Panel")
+        {
+            IPanel newPanel = new DefaultPanel(xPosition, yPosition, width, height, panelName);
+            newPanel.SelectionTool = new SelectionTool();
+            newPanel.SelectionTool.PanelContainer = this.panelContainer;
+            this.panelContainer.AddPanel(newPanel);
+            this.panelContainer.SetActivePanel(newPanel);
+        }
+    }
+}
--- WeeToons/WeeToons/WeetoonsForm.cs
+++ WeeToons/WeeToons/WeetoonsForm.cs
@@ -41,55 +41,25 @@ namespace WeeToons
             ITool onePanelLayout = new OnePanelLayout();
             ITool twoPanelLayout = new TwoPanelLayout();
             ITool threePanelLayout = new ThreePanelLayout();
+            ITool fourPanelLayout = new FourPanelLayout();
             ITool parkBackground = new ParkBackground();
             ITool studentCharacter = new StudentCharacter();
 
             onePanelLayout.PanelContainer = this.panelGroupContainer;
             twoPanelLayout.PanelContainer = this.panelGroupContainer;
             threePanelLayout.PanelContainer = this.panelGroupContainer;
+            fourPanelLayout.PanelContainer = this.panelGroupContainer;
             parkBackground.PanelContainer = this.panelGroupContainer;
             studentCharacter.PanelContainer = this.panelGroupContainer;
 
             panelTool.AddTool(onePanelLayout);
             panelTool.AddTool(twoPanelLayout);
             panelTool.AddTool(threePanelLayout);
+            panelTool.AddTool(fourPanelLayout);
             backgroundTool.AddTool(parkBackground);
             characterTool.AddTool(studentCharacter);
             #endregion
         }
-
-        //private void onePanelToolStrip_Click(object sender, EventArgs e)
-        //{
-        //    removeAllPanel();
-        //    IPanel newPanel = new DefaultPanel(this, 49, 37, 660, 660, "Full Single Panel");
-        //    activatePanel(newPanel);
-        //}
-
-        //private void twoPanelToolStrip_Click(object sender, EventArgs e)
-        //{
-        //    removeAllPanel();
-        //    IPanel leftPanel = new DefaultPanel(this, 49, 37, 320, 320, "left double panel");
-        //    IPanel rightPanel = new DefaultPanel(this, 375, 37, 320, 320, "right double panel");
-        //    activatePanel(leftPanel);
-        //}
-
-        //private void threePanelToolStrip_Click(object sender, EventArgs e)
-        //{
-        //    removeAllPanel();
-        //    IPanel leftPanelTop = new DefaultPanel(this, 49, 37, 200, 200, "Top Left Triple Panel");
-        //    IPanel leftPanelBottom = new DefaultPanel(this, 49, 257, 200, 200, "Bottom Left Triple Panel");
-        //    IPanel rightPanel = new DefaultPanel(this, 265, 37, 420, 420, "Right Triple Panel");
-        //    activatePanel(leftPanelTop);
-        //}
-
-        //private void fourPanelToolStrip_Click(object sender, EventArgs e)
-        //{
-        //    removeAllPanel();
-        //    IPanel leftTopPanel = new DefaultPanel(this, 49, 37, 320, 320, "Top Left Quartet Panel");
-        //    IPanel rightTopPanel = new DefaultPanel(this, 375, 37, 320, 320, "Top Right Quartet Panel");
-        //    IPanel leftBottomPanel = new DefaultPanel(this, 47, 370, 320, 320, "Bottom Left Quartet Panel");
-        //    IPanel rightBottomPanel = new DefaultPanel(this, 375, 370, 320, 320, "Bottom Right Quartet Panel");
-        //    activatePanel(leftTopPanel);
-        //}
+        
     }
 }
