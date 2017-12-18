d70abaf861c925d90fa7d85e20f008477bd17d21
aldorachmansyah <aldorachmansyah@gmail.com>
2017-12-19 04:45:17 +0700

    texting

---
+++ WeeToons/WeeToons/Tools/Text Tools/Text.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeeToons.Interfaces;
+using WeeToons.KomikObjects;
+
+namespace WeeToons.Tools.Property_Tools
+{
+    class Text : ToolStripMenuItem, ITool
+    {
+        private IPanelContainer panelContainer;
+
+        public IPanelContainer PanelContainer
+        {
+            get
+            {
+                return this.panelContainer;
+            }
+
+            set
+            {
+                this.panelContainer = value;
+            }
+        }
+
+        public Ball()
+        {
+            this.Text = "Ball";
+            this.Name = "ballToolStrip";
+            this.Image = Bitmap.FromFile(@"..\..\..\Resources\Property\football.png");
+            this.Click += new EventHandler(this.tool_Click);
+        }
+
+        public void tool_Click(object sender, EventArgs e)
+        {
+            IPanel panel = this.panelContainer.ActivePanel;
+            if (panel != null)
+            {
+                BallProperty ball = new BallProperty();
+                panel.AddComicObject((KomikObject)ball);
+            }
+        }
+    }
+
+}
---
+++ WeeToons/WeeToons/Tools/Text Tools/TextTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using WeeToons.Interfaces;
+using WeeToons.Tools;
+
+namespace WeeToons.Tools.Text_Tools
+{
+    class TextTools : ToolDropDownObject
+    {
+        public TextTools()
+        {
+            this.Name = "textToolStrip";
+            this.Text = "Text";
+            this.Image = Bitmap.FromFile(@"..\..\..\Resources\Icon\text.png");
+        }
+    }
+}
