85604740e3f7bad3e5d471d6cbccf3a43cf98b01
Ario Bimo <hariyosoariobimo@gmail.com>
2017-12-19 04:20:36 +0700

    Composite Experiment

--- WeeToons/WeeToons/ObjectGroup.cs
+++ WeeToons/WeeToons/ObjectGroup.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace WeeToons
 {
-    class ObjectGroup
+    class ObjectGroup : KomikObject
     {
+       private List<KomikObject> objects;
+
+        public override void Draw()
+        {
+            foreach(KomikObject obj in objects)
+            {
+                obj.Draw();
+            }
+        }
+
+        public override void RenderOnEditingView()
+        {
+            foreach (KomikObject obj in objects)
+            {
+                obj.RenderOnEditingView();
+            }
+        }
+
+        public override void Translate(int x, int y, int xAmount, int yAmount)
+        {
+            foreach (KomikObject obj in objects)
+            {
+                obj.Translate(x, y, xAmount, yAmount);
+            }
+        }
+
+        public override void RenderOnStaticView()
+        {
+            foreach (KomikObject obj in objects)
+            {
+                obj.RenderOnStaticView();
+            }
+        }
+
+        public override bool Intersect(int xTest, int yTest)
+        {
+            foreach (KomikObject obj in objects)
+            {
+                if(obj.Intersect(xTest,yTest))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
